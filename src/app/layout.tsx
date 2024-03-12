44c9c6e5c42ee8d094f82b877a6c8048883b70f1
Dokyun Claire Lim <im.koremp@gmail.com>
2024-03-12 15:40:29 +0900

    feat: update app/layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,16 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import './globals.css'
+import { CssBaseline } from '@mui/material'
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Korean Out-of-School Assistance',
-  description: '',
+  title: '한국 청소년 도우미',
 }
 
 export default function RootLayout({
@@ -16,6 +20,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <CssBaseline />
       <body className={inter.className}>{children}</body>
     </html>
   )
